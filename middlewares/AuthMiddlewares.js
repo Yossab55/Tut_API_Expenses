7cde5616a03753678c615c29123a24b36933bfec
Yossab_Samuel <yossabpro5@gmail.com>
2025-08-02 10:39:52 +0300

    refactor: chaning the logic of callback funciton on error to await syntax

--- middlewares/AuthMiddlewares.js
+++ middlewares/AuthMiddlewares.js
@@ -1,18 +1,25 @@
 import JWT from "jsonwebtoken";
+import { makeItPromisify } from "../utils/helpers.js";
+// if there is any error just remove it
+const verify = makeItPromisify(JWT.verify);
 
 const requiredAuth = async function (req, res, next) {
   const token = req.cookies.token;
-  //Todo you can promisfy JWT.verify
-  //Todo don't forget to setup JWT_SECRET
+  //I guess you can make try catch wrapper
   if (token) {
-    await JWT.verify(token, JWT_SECRET, (tokenError, tokenDecoded) => {
+    const tokenDecoded = await verify(token, JWT_SECRET);
+    req.user.id = tokenDecoded.id;
+    return next();
+    /**
+     * (tokenError, tokenDecoded) => {
       if (tokenError) {
         // redirect
         return;
       }
       req.user.id = tokenDecoded.id;
       return next();
-    });
+    }
+     */
   }
   //redirect;
   return;
