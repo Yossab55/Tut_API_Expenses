64990a9ac9b0f7f568718a58dc36db575d8bcaab
Yossab_Samuel <yossabpro5@gmail.com>
2025-08-02 10:40:20 +0300

    abstract promisify funciton

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,13 @@
+import "util/promisify";
+
 function env(field) {
   return process.env[field] || null;
 }
 
 export { env };
+
+function makeItPromisify(fn) {
+  return promisify(fn);
+}
+
+export { makeItPromisify };
