030fd1240a803a4ebbdb1a24597f1721deb5c7ae
Yossab_Samuel <yossabpro5@gmail.com>
2025-08-31 10:17:34 +0300

    delete line of code of userSchema because of i don't need Auto increment id

--- source/validation/schema/UserSchema.js
+++ source/validation/schema/UserSchema.js
@@ -1,7 +1,6 @@
 import { Joi } from "../../interface/joi.js";
 
 const UserSchema = Joi.createObject({
-  // user_id: Joi.number().required(),
   user_email: Joi.email().required(),
   user_name: Joi.between(2, 70).required(),
   password: Joi.string(),
